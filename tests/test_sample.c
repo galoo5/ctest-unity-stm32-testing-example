4818219d6c1f0ff967b00ab569a0506464f009cf
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2025-09-21 19:48:17 +0300

    Add initial project setup with CMake, Unity and CLION configurations.

---
+++ tests/test_sample.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "unity/unity.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+static void test_2x2_still_eq_4(void) {
+    int val = 2*2;
+    TEST_ASSERT_EQUAL_INT(4, val);
+}
+
+static void test_2x2_still_ne_5(void) {
+    TEST_ASSERT_EQUAL_INT(5, 2*2);
+}
+
+int main(void) {
+    UnityBegin(__FILE__);
+    RUN_TEST(test_2x2_still_eq_4);
+    RUN_TEST(test_2x2_still_ne_5);
+    exit(UnityEnd()) ;
+}
